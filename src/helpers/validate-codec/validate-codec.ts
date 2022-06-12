bfac12d8ff4e9fdea70b462478fbdee0689981a6
DiogoJorge1401 <diogojorge1401@gmail.com>
2022-06-12 14:04:23 -0300

    feat(helpers): add validation of a codec's fields

---
+++ src/helpers/validate-codec/validate-codec.ts
@@ -0,0 +1,16 @@
+import * as E from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { failure } from 'io-ts/PathReporter';
+import { Type } from 'io-ts';
+import { ValidationError } from '@/helpers/errors';
+
+type ValidateCodec = <A, O, I>(codec: Type<A, O, I>) =>
+	(data: I) => E.Either<ValidationError, O>;
+
+export const validateCodec: ValidateCodec = (codec) => (data) => {
+	return pipe(
+		codec.decode(data),
+		E.map(codec.encode),
+		E.mapLeft(errors => new ValidationError(failure(errors).join(':::'))),
+	);
+};
